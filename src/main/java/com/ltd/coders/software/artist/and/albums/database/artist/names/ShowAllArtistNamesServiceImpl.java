674abc45ca389a8a8d64ed7aa7a8cd78b22ac841
JasonLeeGit <software.coders.ltd@gmail.com>
2025-01-27 12:06:06 +0000

    removed caching from controller

--- src/main/java/com/ltd/coders/software/artist/and/albums/database/artist/names/ShowAllArtistNamesServiceImpl.java
+++ src/main/java/com/ltd/coders/software/artist/and/albums/database/artist/names/ShowAllArtistNamesServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.ltd.coders.software.artist.and.albums.database.entity.Artist;
