0d9e2be74842eec35f44d831981da79e2a830efe
JasonLeeGit <software.coders.ltd@gmail.com>
2025-02-07 08:13:53 +0000

    removed unused imports

--- src/test/java/com/ltd/coders/software/artist/and/albums/database/insert/artist/InsertNewArtistControllerMockTest.java
+++ src/test/java/com/ltd/coders/software/artist/and/albums/database/insert/artist/InsertNewArtistControllerMockTest.java
@@ -12,14 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 import com.ltd.coders.software.artist.and.albums.database.RepositoryForMocksHelper;
 import com.ltd.coders.software.artist.and.albums.database.dto.ArtistRequestDto;
